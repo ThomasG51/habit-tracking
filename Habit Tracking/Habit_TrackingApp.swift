bd2cd14b321e4929648c3d065af9542aea5cc20b
Thomas George <private@thomas-george.com>
2021-09-09 18:39:46 +0200

    Initial Commit

---
+++ Habit Tracking/Habit_TrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackingApp.swift
+//  Habit Tracking
+//
+//  Created by Thomas George on 09/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
