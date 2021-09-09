439559a68891dd35b5fcb67663a00943e6d79198
Thomas George <private@thomas-george.com>
2021-09-09 19:23:24 +0200

    creation de la liste des activités et du formulaire d'ajout dans une sheet

--- Habit Tracking/ContentView.swift
+++ Habit Tracking/ContentView.swift
@@ -7,10 +7,57 @@
 
 import SwiftUI
 
+struct activitie {
+    let title: String
+    let description: String
+}
+
 struct ContentView: View {
+    @State private var activities: [activitie] = [activitie]()
+    @State private var showingAddActivitie: Bool = false
+    
+    @State private var activitieTitle: String = ""
+    @State private var activitieDescription: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                Text("List of activities")
+            }
+            .navigationBarTitle(Text("Activities"))
+            .navigationBarItems(trailing:
+                Button(
+                    action:{
+                        self.showingAddActivitie = true
+                    },
+                    label: {
+                        Image(systemName: "plus")
+                    }
+                )
+            )
+        }
+        .sheet(isPresented: $showingAddActivitie) {
+            NavigationView {
+                Form {
+                    Section(header: Text("Title")) {
+                        TextField("Title", text: $activitieTitle)
+                    }
+                    
+                    Section(header: Text("Description")) {
+                        TextField("Description", text: $activitieDescription)
+                    }
+                }
+                .navigationBarTitle(Text("Add activitie"))
+                .navigationBarItems(trailing:
+                    Button(
+                        action: {},
+                        label: {
+                            Text("Save")
+                        }
+                    )
+                )
+            }
+        }
     }
 }
 
